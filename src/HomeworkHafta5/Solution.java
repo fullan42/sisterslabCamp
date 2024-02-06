95d939399b631957a1ca44a2973f621ad30eca9b
fullan42 <hasan_-1@outlook.com>
2024-02-06 13:42:24 +0000

    ss

--- src/HomeworkHafta5/Solution.java
+++ src/HomeworkHafta5/Solution.java
@@ -12,4 +12,32 @@ class Solution {
 
         return expectedSum - actualSum;
     }
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int currentArea = minHeight * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+    public int reverse(int x) {
+        int ters = 0;
+        while (x != 0) {
+            int kalan = x % 10;
+            ters = ters * 10 + kalan;
+            x = x / 10;
+        }
+        return ters;
+    }
 }
